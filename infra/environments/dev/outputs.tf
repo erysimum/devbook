5c5584dfe6e9267d54215e37b41cf3172f7328ef
erysimum <cheirierysimum@gmail.com>
2025-11-27 13:34:57 +1100

    Modified terraform code to facilitate the provision of resources

--- infra/environments/dev/outputs.tf
+++ infra/environments/dev/outputs.tf
@@ -1,80 +1,47 @@
-############VPC OUTPUTS###########
-output "vpc_id"                 { 
-    value = module.vpc.vpc_id 
-    }
-output "private_subnets"        { 
-    value = module.vpc.private_subnets 
-    }
-output "public_subnets"         { 
-    value = module.vpc.public_subnets 
-    }
-output "vpc_cidr_block" {
-  value = module.vpc.vpc_cidr_block
+output "vpc_id" {
+  value = module.vpc.vpc_id
 }
 
-output "private_route_table_ids" {
-  value = module.vpc.private_route_table_ids
+output "private_subnets" {
+  value = module.vpc.private_subnets
 }
 
-output "public_route_table_ids" {
-  value = module.vpc.public_route_table_ids
+output "public_subnets" {
+  value = module.vpc.public_subnets
 }
 
-output "internet_gateway_id" {
-  value = module.vpc.igw_id
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
 }
 
-
-
-
-
-##############CLUSTER############
-
-output "cluster_name"           { 
-    value = module.eks.cluster_name 
-    }
-output "cluster_oidc_provider"  { 
-    value = module.eks.oidc_provider_arn 
-    }
-output "node_sg_id"             { 
-    value = module.eks.node_security_group_id
-     }
-
-
-
-
-output "cluster_endpoint" {
+output "eks_cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
-output "cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
+output "eks_cluster_arn" {
+  value = module.eks.cluster_arn
 }
 
-output "eks_node_role_arn" {
-  value = module.eks.node_groups["on_demand"].iam_role_arn
-}
-# cluster_endpoint is needed to configure kubectl.
-# cluster_security_group_id is useful if other resources (like RDS) need to reference it.
-
-#############ECR#########################
-
-output "ecr_frontend_repo_url" {
-  value = aws_ecr_repository.repos["frontend"].repository_url
+output "eks_node_security_group_id" {
+  value = module.eks.node_security_group_id
 }
 
-output "ecr_backend_repo_url" {
-  value = aws_ecr_repository.repos["backend"].repository_url
+output "alb_security_group_id" {
+  value = aws_security_group.alb_sg.id
 }
 
-################RDS##############
-output "rds_endpoint"           { value = module.rds.db_instance_address }
-output "rds_db_name"            { value = module.rds.db_instance_name }
-output "rds_port" {
-  value = module.rds.db_instance_port
+output "backend_security_group_id" {
+  value = aws_security_group.backend_sg.id
 }
 
-output "rds_security_group_ids" {
-  value = module.rds.db_security_group_ids
+output "rds_endpoint" {
+  value = module.rds.db_instance_endpoint
 }
 
+output "alb_irsa_role_arn" {
+  value = module.alb_irsa.iam_role_arn
+}
+
+output "ecr_repositories" {
+  value = [for r in aws_ecr_repository.repos : r.repository_url]
+}
--- infra/environments/dev/variables.tf
+++ infra/environments/dev/variables.tf
@@ -1,115 +1,91 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
 variable "project_name" {
-  description = "Project name for naming and tagging"
+  description = "Project name"
   type        = string
 }
 
 variable "env" {
-  description = "Environment (e.g. dev, staging, prod)"
+  description = "Environment (dev/staging/prod)"
   type        = string
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "region" {
-  description = "Region"
-  type        = string
-  default     = "ap-southeast-2"
 }
 
 variable "owner" {
-  description = "Owner or team responsible for the resources"
+  description = "Owner of resources"
   type        = string
-  default     = "DevOpsTeam"
 }
 
+#vpc Networking variable
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
 
-
-
-#######################################################
-##########EKS#####################################
+#EKS Variables
 variable "eks_version" {
   description = "EKS cluster version"
   type        = string
-  default     = "1.30"
 }
 
 variable "node_instance_type" {
-  description = "Default EC2 instance type for worker nodes"
+  description = "EKS worker node instance type"
   type        = string
-  default     = "t3.small"
 }
 
 variable "node_min_size" {
-  description = "Minimum number of nodes"
+  description = "Minimum number of worker nodes"
   type        = number
-  default     = 2
 }
 
 variable "node_max_size" {
-  description = "Maximum number of nodes"
+  description = "Maximum number of worker nodes"
   type        = number
-  default     = 3
 }
 
 variable "node_desired_size" {
-  description = "Desired number of nodes"
+  description = "Desired number of worker nodes"
   type        = number
-  default     = 2
 }
 
-variable "enable_public_endpoint" {
-  description = "Enable public access to EKS API endpoint"
-  type        = bool
-  default     = false
-}
-
-variable "use_bottlerocket" {
-  description = "Use Bottlerocket AMI instead of AL2"
-  type        = bool
-  default     = false
-}
-
-############ECR############
-variable "ecr_repos" {
-  type    = list(string)
-  default = ["frontend", "backend"]
-  description = "List of ECR repositories to create"
-}
-
-variable "image_retention_count" {
-  type    = number
-  default = 2
-  description = "Number of images to keep in ECR"
-}
-
-variable "image_tag_mutability" {
-  type    = string
-  default = "MUTABLE"
-}
-variable "force_delete" {
-  type    = bool
-  default = true
-}
-
-######################################
+#DB Variable
 variable "db_name" {
-  description = "Name of the PostgreSQL database"
+  description = "Database name"
   type        = string
-  default     = "devbook"
 }
 
 variable "db_username" {
-  description = "Master username for the database"
+  description = "Database username"
   type        = string
-  default     = "devbook"
 }
 
 variable "db_password" {
-  description = "Password for the PostgreSQL database"
+  description = "Database password"
   type        = string
   sensitive   = true
+}
+
+#ECR Variable
+variable "ecr_repos" {
+  description = "List of ECR repositories"
+  type        = list(string)
+}
+
+variable "image_retention_count" {
+  description = "Number of images to keep in ECR"
+  type        = number
+}
+
+variable "image_tag_mutability" {
+  description = "ECR image tag mutability"
+  type        = string
+  default     = "MUTABLE" #Whether image tags can be overwritten
+}
+
+variable "force_delete" {
+  description = "Force delete ECR repository if not empty"
+  type        = bool
+  default     = false #Whether Terraform should force delete the ECR repository even if it’s not empty
 }
